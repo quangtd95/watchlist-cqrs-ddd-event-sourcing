22015b16bfe450f1c23ca3a66f7884772c2cd625
quangthaiduy <quang@workforceoptimizer.com>
2023-10-30 10:21:18 +0700

    reactive controller

--- watchlist/src/main/kotlin/com/qstudio/investing/watchlist/adapter/rest/v1/WatchlistController.kt
+++ watchlist/src/main/kotlin/com/qstudio/investing/watchlist/adapter/rest/v1/WatchlistController.kt
@@ -1,9 +1,11 @@
 package com.qstudio.investing.watchlist.adapter.rest.v1
 
-import com.qstudio.investing.watchlist.adapter.rest.v1.response.BaseResponse
 import com.qstudio.investing.watchlist.adapter.rest.v1.request.CreateWatchListRequest
 import com.qstudio.investing.watchlist.adapter.rest.v1.request.RenameWatchListRequest
+import com.qstudio.investing.watchlist.adapter.rest.v1.response.BaseResponse
 import com.qstudio.investing.watchlist.adapter.rest.v1.response.CreateWatchListResponse
+import com.qstudio.investing.watchlist.adapter.rest.v1.response.GetAllWatchlistResponse
+import com.qstudio.investing.watchlist.adapter.rest.v1.response.GetWatchlistResponse
 import com.qstudio.investing.watchlist.core.command.type.CreateWatchlistCommand
 import com.qstudio.investing.watchlist.core.command.type.RenameWatchlistCommand
 import com.qstudio.investing.watchlist.core.query.model.WatchlistView
@@ -12,11 +14,14 @@ import com.qstudio.investing.watchlist.core.query.type.WatchlistGetByIdQuery
 import jakarta.validation.Valid
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.axonframework.messaging.responsetypes.ResponseTypes
 import org.axonframework.queryhandling.QueryGateway
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
+import reactor.kotlin.core.publisher.toMono
 import java.util.*
 
 @RestController
@@ -27,46 +32,49 @@ class WatchlistController(
     private val log = LoggerFactory.getLogger(WatchlistController::class.java)
 
     @GetMapping("/{watchlistId}")
-    fun getWatchlist(@Valid @PathVariable watchlistId: String): BaseResponse<WatchlistView> {
+    suspend fun getWatchlist(@Valid @PathVariable watchlistId: String): BaseResponse<GetWatchlistResponse> {
         log.info("querying watchlist: $watchlistId")
         val watchlist = queryGateway.query(
             WatchlistGetByIdQuery(watchlistId),
             WatchlistView::class.java
-        ).get()
-        return BaseResponse.success(watchlist)
+        ).toMono().awaitSingleOrNull()
+
+        return BaseResponse.success(GetWatchlistResponse(watchlist))
     }
 
     @GetMapping
-    fun getAllWatchlist(): BaseResponse<List<WatchlistView>> {
+    suspend fun getAllWatchlist(): BaseResponse<GetAllWatchlistResponse> {
         log.info("querying all watchlist")
-        val watchlist = queryGateway.query(
+        val watchlists = queryGateway.query(
             WatchlistGetAllQuery(), ResponseTypes.multipleInstancesOf(WatchlistView::class.java)
-        ).get()
-        return BaseResponse.success(watchlist)
+        ).toMono().awaitSingle()
+        return BaseResponse.success(GetAllWatchlistResponse(watchlists))
+
     }
 
     @PostMapping
-    suspend fun createWatchlist(@Valid @RequestBody request: CreateWatchListRequest): BaseResponse<Map<String, CreateWatchListResponse>> {
+    suspend fun createWatchlist(@Valid @RequestBody request: CreateWatchListRequest): BaseResponse<CreateWatchListResponse> {
         log.info("creating watchlist: $request; requestId = ${request.requestId}")
 
-        val watchlistId = commandGateway.sendAndWait<String>(
+
+        val newId = commandGateway.send<String>(
             CreateWatchlistCommand(
                 watchlistId = UUID.randomUUID().toString(),
                 userId = request.userId,
                 name = request.name
             )
-        )
-        return BaseResponse.success(mapOf("watchlist" to CreateWatchListResponse(watchlistId)))
+        ).toMono().awaitSingle()
+        return BaseResponse.success(CreateWatchListResponse(WatchlistView(newId, request.userId, request.name)))
     }
 
     @PatchMapping("/{watchlistId}/rename")
-    fun renameWatchlist(
+    suspend fun renameWatchlist(
         @Valid @PathVariable @NotNull @NotBlank watchlistId: String,
         @Valid @RequestBody request: RenameWatchListRequest
     ): BaseResponse<Map<String, String>> {
         log.info("renaming watchlist: $request; requestId = ${request.requestId}")
 
-        commandGateway.send<Any>(RenameWatchlistCommand(watchlistId, request.newName))
+        commandGateway.send<Any>(RenameWatchlistCommand(watchlistId, request.newName)).toMono().awaitSingle()
 
         return BaseResponse.success(mapOf("result" to "rename request accepted"))
     }
--- watchlist/src/main/kotlin/com/qstudio/investing/watchlist/adapter/rest/v1/response/GetAllWatchlistResponse.kt
+++ watchlist/src/main/kotlin/com/qstudio/investing/watchlist/adapter/rest/v1/response/GetAllWatchlistResponse.kt
@@ -2,4 +2,4 @@ package com.qstudio.investing.watchlist.adapter.rest.v1.response
 
 import com.qstudio.investing.watchlist.core.query.model.WatchlistView
 
-data class CreateWatchListResponse(val watchlist: WatchlistView)
+data class GetAllWatchlistResponse(val watchlists: List<WatchlistView>)
